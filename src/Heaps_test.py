e97ebdfe88a53ca224441686125bf1f390894d64
alvin <alvinjericho@gmail.com>
2022-03-15 21:12:04 -0700

    Staged Heap code

---
+++ src/Heaps_test.py
@@ -0,0 +1,38 @@
+from Heaps import MaxHeaps, MinHeaps   # The code to test
+import unittest   # The test framework
+
+
+class Test_Heaps(unittest.TestCase):
+    def setUp(self):
+        pass
+      
+    def testgetMin(self):
+        pass
+      
+    def testgetMax(self):
+        pass  
+    
+    def testExtractMin(self):
+        pass
+
+    def testExtractMax(self):
+        pass
+
+    def testDecreaseKey(self):
+        pass
+        
+    def testInsertOnMinHeap(self):
+        pass
+    
+    def testInsertOnMaxHeap(self):
+        pass    
+    
+    def testDeleteOnMinHeap(self):
+         pass
+        
+    def testDeleteOnMaxHeap(self):
+        pass
+    
+
+if __name__ == '__main__':
+    unittest.main()
